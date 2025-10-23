4b56d7ec05f5046e0f773987bcbb171053369958
Renan1919 <63660044+Renan1919@users.noreply.github.com>
2025-10-23 00:51:36 +0000

    adicionei arquivo operadores.js

---
+++ aulas/javascript/operadores.js
@@ -0,0 +1,38 @@
+/** OPERADORES ARITMÉTICOS */
+// + - * / % **
+
+/** OUTROS OPERADORES */
+// > >= < <= != (maior, maior que, menor, menor que, diferente)
+// && || (E lógico, OU lógico)
+/*
+const ehPar = 4 // Number
+
+if(ehPar % 2 == 0) {
+        // trata quando a condição for verdadeira
+    alert('O número ' + ehPar + ' é par!')
+} else {
+    // trata a condição quando a falsa
+    alert('O número ' + ehPar + ' é ímpar!')
+}
+
+console.log(ehPar % 2 == 0)
+*/
+
+/*
+// Pessoas com idade entre 18 e 70 anos podem entrar.
+// Pessoas com maiores de 70 precisa de companhia
+// Menores, vazam!
+
+const idade = 71
+
+if(idade >= 18 && idade <= 70) {
+    alert('Você tem ' + idade +' anos \nPode entrar no baile')
+} else if(idade < 70){
+    alert('Você tem ' + idade +' anos \nPrecisa de um acompanhante para entrar no baile!')
+} else if(idade < 18){
+    alert('Vaza, menor!')
+} 
+else {
+    alert('Por favor digite um número para idade!')
+}
+*/
