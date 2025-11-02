86d1f2878dabc6c23e4ddbb398f92d067cd6472f
Renan1919 <63660044+Renan1919@users.noreply.github.com>
2025-11-02 22:34:55 +0000

    funções de cálculo subtração, multiplicação, divisão: adiciona verificação para divisão por zero

--- aulas/javascript/funcoes.js
+++ aulas/javascript/funcoes.js
@@ -12,24 +12,53 @@
 // function dizOlaPessoa(nome) {
 //     alert(`Não seja tímido(a)!' ${nome}. 'Fala oi!`)
 // }
-const jogadores = ['Neymar', 'Ronaldinho', 'Messi']
-const frutas = ['banana', 'maçã', 'morango']
+// const jogadores = ['Neymar', 'Ronaldinho', 'Messi']
+// const frutas = ['banana', 'maçã', 'morango']
 
-function formataTexto(lista){
-    for(let texto of lista){
-        console.log(texto.toUpperCase())
-        // texto.toLowerCase() -> deixa tudo minúsculo
-        // texto.trim() -> remove espaços em branco
-        // texto.join(' outra coisa ') -> junta com outro texto
-    }
-}
+// function formataTexto(lista){
+//     for(let texto of lista){
+//         console.log(texto.toUpperCase())
+//         // texto.toLowerCase() -> deixa tudo minúsculo
+//         // texto.trim() -> remove espaços em branco
+//         // texto.join(' outra coisa ') -> junta com outro texto
+//     }
+// }
 // formataTexto(jogadores)
+
+// --- Função de Adição ---
 function adicao(n1 , n2){
     return n1 + n2
 }
-// faça uma versão para subtracao, multiplicacao e divisao 
-const resultadoAdicao = adicao(20, 5)  + 30
+const resultadoAdicao = adicao(100, 5)  + 30
 console.log(`Adição: ${resultadoAdicao}`)
+
+
+// --- Subtração ---
+function subtracao(n1, n2) {
+    return n1 - n2
+}
+const resultadoSubtracao = subtracao(1000, 599) + 100
+console.log(`Subtração: ${resultadoSubtracao}`)
+
+
+// --- Multiplicação ---
+function multiplicacao(n1, n2) {
+    return n1 * n2
+}
+const resultadoMultiplicacao = multiplicacao(10, 50)
+console.log(`Multiplicação: ${resultadoMultiplicacao}`)
+
+
+// --- Divisão ---
+function divisao(n1, n2) {
+    // Adiciona uma verificação para não dividir por zero
+    if (n2 === 0) {
+        return "Erro: Divisão por zero!"
+    }
+    return n1 / n2
+}
+const resultadoDivisao = divisao(500, 5)
+console.log(`Divisão: ${resultadoDivisao}`)
 
 
 // Invocar ou chamar a função
